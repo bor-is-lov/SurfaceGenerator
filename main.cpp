2e5a93775cb81adab937b6844db466b908b2e8a2
bor-is-lov <b.o.r_k.o.r@mail.ru>
2024-04-02 10:56:06 +0300

    first working build

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION //stb_image_write.h requires this
 #define __STDC_LIB_EXT1__ //it has to be defined for stb_image_write.h to work with C++ (may be a bug)
 #include "stb_image_write.h"
-#define DEBUG 0
+#define DEBUG 1
 
 struct Color{
 	unsigned char red = 0;
@@ -14,102 +14,69 @@ struct Color{
 };
 
 int main(){
-	srand(time(NULL));
+	srand(1);
 	size_t height, width;
 	std::cout << "Enter the image resolution (each pixel is a block):\nWidth: ";
 	std::cin >> width;
 	std::cout << "Height: ";
 	std::cin >> height;
 #if DEBUG == 1
-	auto start = chrono::high_resolution_clock::now();
+	auto start = std::chrono::high_resolution_clock::now();
 #endif // DEBUG
-	std::vector<std::vector<double>> land(height, std::vector<double>(width));
-	std::vector<std::vector<double>> tempLand(height, std::vector<double>(width));
-	std::vector<std::vector<double>> land2(height, std::vector<double>(width));
+	std::vector<std::vector<double>>
+		src(height, std::vector<double>(width)),
+		land(height, std::vector<double>(width)),
+		land2(height, std::vector<double>(width));
 
 	for (size_t y = 0; y < height; ++y)
 		for (size_t x = 0; x < width; ++x)
-		{
-			land[x][y] = (rand() % 201) / 100.0 - 1;//filling land with random values from -1 to 1
-			land2[x][y] = (rand() % 201) / 100.0 - 1;//filling land with random values from -1 to 1
-		}
+			src[x][y] = (rand() % 201) / 100.0 - 1;//filling land with random values from -1 to 1
 
 	//smoothing
-	auto smooth = [&](int x1, int x2, std::vector<std::vector<double>> land){
-		for (size_t y = 0; y < height; ++y)
-			for (size_t x = x1; x < x2; ++x){
-				double sum = land[y][x];
-				int amount = 1;
-				if (x != 0){
-					++amount;
-					sum += land[y][x - 1];
-				}
-				if (y != 0){
-					++amount;
-					sum += land[y - 1][x];
-				}
-				if (x != width - 1){
-					++amount;
-					sum += land[y][x + 1];
-				}
-				if (y != height - 1){
-					++amount;
-					sum += land[y + 1][x];
-				}
-				if (x != 0 && y != 0){
-					++amount;
-					sum += land[y - 1][x - 1];
-				}
-				if (x != 0 && y != height - 1){
-					++amount;
-					sum += land[y + 1][x - 1];
-				}
-				if (x != width - 1 && y != height - 1){
-					++amount;
-					sum += land[y + 1][x + 1];
-				}
-				if (x != width - 1 && y != 0){
-					++amount;
-					sum += land[y - 1][x + 1];
-				}
-				tempLand[y][x] = sum / amount;
+	auto smooth = [&](std::vector<std::vector<double>>* land, int x1, int x2, int radius){
+		for (int y = 0; y < height; ++y)
+			for (int x = x1; x < x2; ++x)
+			{
+				double sum = 0;
+				int amount = 0;
+				for (int yr = y - radius; yr < y + radius; yr++)
+					for (int xr = x - radius; xr < x + radius; xr++)
+					{
+						if (xr < 0 || yr < 0 || xr >= width || yr >= height)
+							continue;
+						double influence = 1 - sqrt((x - xr) * (x - xr) + (y - yr) * (y - yr)) / radius;
+						if (influence > 0.0)
+							sum += src[xr][yr] * influence;
+						amount++;
+					}
+				(*land)[x][y] = sum / amount;
 			}
 	};
 	int thSize = std::thread::hardware_concurrency() - 1;
 	if (thSize < 1) return 1;
 	std::thread* threads = new std::thread[thSize];
-	for (size_t i = 0; i < 50; i++){//more times = huger areas, but lower performance
+	{
 		for (size_t j = 0; j < thSize; ++j){
 			int x1 = width / thSize * j;
 			int x2 = (j < thSize - 1) ? width / thSize * (j + 1) : width;
-			threads[j] = std::thread(smooth, x1, x2, land);
+			threads[j] = std::thread(smooth, &land, x1, x2, 100);
 		}
 		for (size_t j = 0; j < thSize; ++j)
 			threads[j].join();
-		for (size_t y = 0; y < height; ++y)
-			for (size_t x = 0; x < width; ++x)
-				land[y][x] = tempLand[y][x];
 	}
-	for (size_t i = 0; i < 500; i++){//more times = huger areas, but lower performance
+	{
 		for (size_t j = 0; j < thSize; ++j){
 			int x1 = width / thSize * j;
 			int x2 = (j < thSize - 1) ? width / thSize * (j + 1) : width;
-			threads[j] = std::thread(smooth, x1, x2, land2);
+			threads[j] = std::thread(smooth, &land2, x1, x2, 10);
 		}
 		for (size_t j = 0; j < thSize; ++j)
 			threads[j].join();
-		for (size_t y = 0; y < height; ++y)
-			for (size_t x = 0; x < width; ++x)
-				land2[y][x] = tempLand[y][x];
 	}
 	delete[] threads;
-	for (auto& row : tempLand)
+	for (auto& row : src)
 		row.~vector();
-	tempLand.~vector();
-
-	for (size_t y = 0; y < height; ++y)
-		for (size_t x = 0; x < width; ++x)
-			land[x][y] = land[x][y] / 10 + land2[x][y];
+	src.~vector();
 
 	//scaling back to [-1; 1]
 	double maxEl = -1;
@@ -121,6 +88,20 @@ int main(){
 	for (auto& row : land)
 		for (auto& block : row)
 			block *= factor;
+
+	maxEl = -1;
+	for (auto& row : land2)
+		for (auto& block : row)
+			if (maxEl < block || maxEl < abs(block))
+				maxEl = abs(block);
+	factor = 1 / maxEl;
+	for (auto& row : land2)
+		for (auto& block : row)
+			block *= factor;
+
+	for (int y = 0; y < height; ++y)
+		for (int x = 0; x < width; ++x)
+			land[x][y] = land[x][y] + land2[x][y]/10;
 
 	//setting colors according to the land height
 	std::vector<std::vector<Color>> img(height, std::vector<Color>(width));
@@ -153,9 +134,9 @@ int main(){
 		std::cout << "Error during creating image\n";
 	delete[] imgData;
 #if DEBUG == 1
-	auto end = chrono::high_resolution_clock::now();
-	chrono::duration<float> duration = end - start;
-	cout << "Duration: " << duration.count() << "\a\n";
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float> duration = end - start;
+	std::cout << "Duration: " << duration.count() << "\a\n";
 #endif // DEBUG
 	return 0;
 }
